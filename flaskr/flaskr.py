61aaa742f90c827de5d8f98d8bfcb21fcec1de71
Rohit Ramesh <rohitramesh47@gmail.com>
2017-12-17 15:21:57 +0530

    Establish database connections

--- flaskr/flaskr.py
+++ flaskr/flaskr.py
@@ -18,3 +18,15 @@ def connect_db():
 	rv = sqlite3.connect(app.config['DATABASE'])
 	rv.row_factory = sqlite3.Row
 	return rv
+
+def get_db():
+	"""Opens a new database connection if there is none yet for the cuurent application context."""
+	if not hasattr(g, 'sqlite_db'):
+		g.sqlite_db = connect_db()
+	return g.sqlite_db
+
+@app.teardown_context
+def close_db(error):
+	"""Closes the database again at the end of the request."""
+	if hasattr(g, 'sqlite_db'):
+		g.sqlite_db.close()
